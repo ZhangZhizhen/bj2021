8bb3c68632eb1174e9866396c2084a2f0ae5e2bb
zzz <zhizhen.zhang@rjoy.com>
2021-04-15 12:28:05 +0800

    Revert "添加viewbind"
    This reverts commit 3766eb73c1e2beb509e7825a5033333d82dc9344.

--- hxyzzz/app/src/main/java/com/oasgames/android/hxyzzz/MainActivity.java
+++ hxyzzz/app/src/main/java/com/oasgames/android/hxyzzz/MainActivity.java
@@ -3,22 +3,15 @@ package com.oasgames.android.hxyzzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.oasgames.android.hxyzzz.databinding.ActivityMainBinding;
-
-/**
- * 应用首页
- */
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
-    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
+        setContentView(R.layout.activity_main);
     }
 
 
