3766eb73c1e2beb509e7825a5033333d82dc9344
zzz <zhizhen.zhang@rjoy.com>
2021-04-15 12:22:32 +0800

    添加viewbind

--- hxyzzz/app/src/main/java/com/oasgames/android/hxyzzz/MainActivity.java
+++ hxyzzz/app/src/main/java/com/oasgames/android/hxyzzz/MainActivity.java
@@ -3,14 +3,21 @@ package com.oasgames.android.hxyzzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.oasgames.android.hxyzzz.databinding.ActivityMainBinding;
+
+/**
+ * 应用首页
+ */
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
     }
 }
